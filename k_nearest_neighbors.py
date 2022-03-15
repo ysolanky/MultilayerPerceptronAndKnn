9864d408802df0e698c85b2c5ad2036d83dc0711
ysolanky <101447028+ysolanky@users.noreply.github.com>
2022-03-15 02:18:55 -0400

    Add files via upload

---
+++ k_nearest_neighbors.py
@@ -0,0 +1,128 @@
+# k_nearest_neighbors.py: Machine learning implementation of a K-Nearest Neighbors classifier from scratch.
+#
+# Submitted by: [enter your full name here] -- [enter your IU username here]
+#
+# Based on skeleton code by CSCI-B 551 Fall 2021 Course Staff
+
+import numpy as np
+from utils import euclidean_distance, manhattan_distance
+
+
+class KNearestNeighbors:
+    """
+    A class representing the machine learning implementation of a K-Nearest Neighbors classifier from scratch.
+
+    Attributes:
+        n_neighbors
+            An integer representing the number of neighbors a sample is compared with when predicting target class
+            values.
+
+        weights
+            A string representing the weight function used when predicting target class values. The possible options are
+            {'uniform', 'distance'}.
+
+        _X
+            A numpy array of shape (n_samples, n_features) representing the input data used when fitting the model and
+            predicting target class values.
+
+        _y
+            A numpy array of shape (n_samples,) representing the true class values for each sample in the input data
+            used when fitting the model and predicting target class values.
+
+        _distance
+            An attribute representing which distance metric is used to calculate distances between samples. This is set
+            when creating the object to either the euclidean_distance or manhattan_distance functions defined in
+            utils.py based on what argument is passed into the metric parameter of the class.
+
+    Methods:
+        fit(X, y)
+            Fits the model to the provided data matrix X and targets y.
+
+        predict(X)
+            Predicts class target values for the given test data matrix X using the fitted classifier model.
+    """
+
+    def __init__(self, n_neighbors=5, weights='uniform', metric='l2'):
+        # Check if the provided arguments are valid
+        if weights not in ['uniform', 'distance'] or metric not in ['l1', 'l2'] or not isinstance(n_neighbors, int):
+            raise ValueError('The provided class parameter arguments are not recognized.')
+
+        # Define and setup the attributes for the KNearestNeighbors model object
+        self.n_neighbors = n_neighbors
+        self.weights = weights
+        self._X = None
+        self._y = None
+        self._distance = euclidean_distance if metric == 'l2' else manhattan_distance
+
+    def fit(self, X, y):
+        """
+        Fits the model to the provided data matrix X and targets y.
+
+        Args:
+            X: A numpy array of shape (n_samples, n_features) representing the input data.
+            y: A numpy array of shape (n_samples,) representing the true class values for each sample in the input data.
+
+        Returns:
+            None.
+        """
+        # Fit function just returns the X and y dataset
+        self._X = X
+        self._y = y
+
+    def predict(self, X):
+        """
+        Predicts class target values for the given test data matrix X using the fitted classifier model.
+
+        Args:
+            X: A numpy array of shape (n_samples, n_features) representing the test data.
+
+        Returns:
+            A numpy array of shape (n_samples,) representing the predicted target class values for the given test data.
+        """
+        result = []
+        for i in X:
+
+            dist = []
+            # We calculate the distance based on the parameter selected
+
+            if self._distance == euclidean_distance:
+                for j, k in enumerate(self._X):
+                    dist.append((euclidean_distance(i, k), j))
+
+            else:
+                for j, k in enumerate(self._X):
+                    dist.append((manhattan_distance(i, k), j))
+
+            sorted_dist = sorted(dist, key=lambda x: int(x[0]))
+
+            # We select the lowest n distances from sorted list, n being the number of neighbours specified.
+            ns = sorted_dist[0:self.n_neighbors]
+
+            temp = {}
+            # We calculate the vote based on type of weight specified.
+
+            if self.weights == "uniform":
+                for i in ns:
+                    if self._y[i[1]] in temp.keys():
+                        temp[self._y[i[1]]] = temp[self._y[i[1]]] + 1
+                    else:
+                        temp[self._y[i[1]]] = 1
+
+            elif self.weights == "distance":
+                for i in ns:
+                    if self._y[i[1]] in temp.keys():
+                        if i[0] != 0:
+                            temp[self._y[i[1]]] = temp[self._y[i[1]]] + 1 / i[0]
+                        else:
+                            temp[self._y[i[1]]] = temp[self._y[i[1]]]
+                    else:
+                        if i[0] != 0:
+                            temp[self._y[i[1]]] = 1 / i[0]
+                        else:
+                            temp[self._y[i[1]]] = 0
+
+            # Start of code from stack overflow https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+            result.append(max(temp, key=temp.get))
+            # End of code from stack overflow
+
+        return result
---
+++ utils.py
@@ -0,0 +1,183 @@
+# utils.py: Utility file for implementing helpful utility functions used by the ML algorithms.
+#
+# Submitted by: [Yash Pratap Solanky] -- [ysolanky]
+#
+# Based on skeleton code by CSCI-B 551 Fall 2021 Course Staff
+
+import numpy as np
+
+def euclidean_distance(x1, x2):
+    """
+    Computes and returns the Euclidean distance between two vectors.
+
+    Args:
+        x1: A numpy array of shape (n_features,).
+        x2: A numpy array of shape (n_features,).
+    """
+
+    return np.sqrt(sum((x1 - x2) ** 2))
+
+
+def manhattan_distance(x1, x2):
+    """
+    Computes and returns the Manhattan distance between two vectors.
+
+    Args:
+        x1: A numpy array of shape (n_features,).
+        x2: A numpy array of shape (n_features,).
+    """
+    return sum(abs(x1 - x2))
+
+
+def identity(x, derivative=False):
+    """
+    Computes and returns the identity activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    # definition of function taken from # https://en.wikipedia.org/wiki/Activation_function
+    if derivative:
+        return np.ones((x.shape[0], x.shape[1]))
+    else:
+        return x
+
+
+def sigmoid(x, derivative=False):
+    """
+    Computes and returns the sigmoid (logistic) activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    # definition of function taken from # https://en.wikipedia.org/wiki/Activation_function
+    # To solve overflow warning I used np.clip https://stackoverflow.com/questions/40726490/overflow-error-in-pythons-numpy-exp-function
+    x = np.clip(x, -709.78, 709.78)
+    # End of code taken from stackoverflow
+
+    temp = 1 / (1 + np.exp(-x))
+
+    if derivative:
+        return temp * (1 - temp)
+    else:
+        return temp
+
+
+def tanh(x, derivative=False):
+    """
+    Computes and returns the hyperbolic tangent activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    # definition of function taken from # https://en.wikipedia.org/wiki/Activation_function
+    # To solve overflow warning I used np.clip https://stackoverflow.com/questions/40726490/overflow-error-in-pythons-numpy-exp-function
+    x = np.clip(x, -709.78, 709.78)
+    # End of code taken from stackoverflow
+
+    a = (np.exp(x) - np.exp(-x))
+    b = (np.exp(x) + np.exp(-x))
+
+    temp = a / b
+
+    if derivative:
+        return 1 - (temp) ** 2
+    else:
+        return temp
+
+
+def relu(x, derivative=False):
+    """
+    Computes and returns the rectified linear unit activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    # To avoid OverflowError: Python int too large to convert to C long, I added the following line of code
+    # from https://stackoverflow.com/questions/38314118/overflowerror-python-int-too-large-to-convert-to-c-long-on-windows-but-not-ma
+    x = np.array(x, dtype=np.int32)
+    # End of code from stack overflow
+
+    relu = lambda t: max(t, 0)
+
+    # Code taken from # https://stackoverflow.com/questions/35215161/most-efficient-way-to-map-function-over-numpy-array
+    vector = np.vectorize(relu)
+    # End of code taken from stack overflow
+
+    # I created the function below taking inspiration from the function above.
+    relu_d = lambda t: 0 if t <= 0 else 1
+
+    vector_d = np.vectorize(relu_d)
+
+    if derivative:
+        return vector_d(x)
+    else:
+        return vector(x)
+
+
+def softmax(x, derivative=False):
+    x = np.clip(x, -1e100, 1e100)
+    if not derivative:
+        c = np.max(x, axis=1, keepdims=True)
+        return np.exp(x - c - np.log(np.sum(np.exp(x - c), axis=1, keepdims=True)))
+    else:
+        return softmax(x) * (1 - softmax(x))
+
+
+def cross_entropy(y, p):
+    """
+    Computes and returns the cross-entropy loss, defined as the negative log-likelihood of a logistic model that returns
+    p probabilities for its true class labels y.
+
+    Args:
+        y:
+            A numpy array of shape (n_samples, n_outputs) representing the one-hot encoded target class values for the
+            input data used when fitting the model.
+        p:
+            A numpy array of shape (n_samples, n_outputs) representing the predicted probabilities from the softmax
+            output activation function.
+    """
+
+    # definition of cross entropy loss taken from https://en.wikipedia.org/wiki/Cross_entropy
+    np.seterr(divide='ignore')
+    np.seterr(invalid="ignore")
+    # ignoring the warnings - https://stackoverflow.com/questions/21610198/runtimewarning-divide-by-zero-encountered-in-log
+
+    return -np.sum(y * np.log(p))
+
+
+def one_hot_encoding(y):
+    """
+    Converts a vector y of categorical target class values into a one-hot numeric array using one-hot encoding: one-hot
+    encoding creates new binary-valued columns, each of which indicate the presence of each possible value from the
+    original data.
+
+    Args:
+        y: A numpy array of shape (n_samples,) representing the target class values for each sample in the input data.
+
+    Returns:
+        A numpy array of shape (n_samples, n_outputs) representing the one-hot encoded target class values for the input
+        data. n_outputs is equal to the number of unique categorical class values in the numpy array y.
+    """
+    temp = np.zeros((len(y), len(np.unique(y))))
+
+    columns = {}
+
+    count = 0
+    for i in np.unique(y):
+        columns[i] = count
+        count += 1
+
+    count = 0
+    for i in y:
+        temp[count][columns[i]] = 1
+        count += 1
+    return temp
